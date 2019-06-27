346165598878c6c00ba16abc2466e69bdf042bc4
Emanuel Lucas de Sousa e Silva <cardgameheritage@gmail.com>
2019-06-26 21:28:15 -0300

    almost there

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-
+const result = require('./service/gitconsume')
+const PREFIX = '!'
 require('dotenv').config()
 
-//const token = 'NTkzNTM2NTM1NzQ4NjczNTM5.XRPXJA.nXFMbUvQbPgM547nQXGIsU2AJv0'
 
 client.on('ready', () => {
     console.log('Bot is online')
 })
 
-client.on('message', msg => {
-    if(msg.content === 'Hello'){
-        msg.reply('Hello friend')
+client.on('message', message => {
+    let args = message.content.substring(PREFIX.length).split(' ')
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('pong!')
+            break
+        case 'help':
+            message.channel.send('Take some coffee...')
+            break
+        case 'info':
+            if(args[1]){
+                message.channel.send('Version 0.1')
+
+            } else {
+                message.channel.send('Invalid argument')
+            }
+        break            
     }
 })
 
