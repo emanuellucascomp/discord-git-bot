b5de258185e1a1c3ef3ce5a416c9e7a6c5631aca
tavares1 <lucastavaresvasconcelos@gmail.com>
2019-06-26 23:54:30 -0300

    removendo validação do process.argv que nao é mais utilizado.

--- service/gitconsumetest.js
+++ service/gitconsumetest.js
@@ -1,22 +1,15 @@
 const result = require('./gitconsume')
 let argv = require('yargs')
-            .option('username', {
-                alias: 'u',
-                describe: 'Example: node gitconsumetest.js -u tavares1'
-            })
-            .demandOption('username')
-            .argv
-
-// Utilizando o argv do node para receber o user do Github via terminal
-if (process.argv == undefined || process.argv == "") {
-    console.log(" ❌ You need to pass a github user. ❌")
-    console.log(" ▶️ Example: node gitconsumetest.js tavares1")
-    return 1
-}
-else {
-    const user = argv.username
-    let message = result.getResponse(user)
-    message.then(res => {
-        console.log(res)
+    .option('username', {
+        alias: 'u',
+        describe: 'Example: node gitconsumetest.js -u tavares1'
     })
-}
+    .demandOption('username')
+    .argv
+
+const user = argv.username
+let message = result.getResponse(user)
+message.then(res => {
+    console.log(res)
+})
+
